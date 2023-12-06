cbb65c508c0ca7e712ab73d9354908030f2a8a72
Ashibaghel44 <138962968+Ashibaghel44@users.noreply.github.com>
2023-12-06 16:20:54 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,71 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'task-manager-app';
+  
+  editIndex :any
+  taskForm!: FormGroup;
+  isSave: boolean = true;
+  isUpdate: boolean = false;
+  taskList: any = [
+    {firstname:'Ashish',mobile:'7894561230', email:'ancd@gmail.com'}
+  ]
+
+  ngOnInit() {
+    this.taskForm = new FormGroup({
+      firstname: new FormControl('', [
+        Validators.required,
+        Validators.pattern('[a-zA-Z ]*')
+      ]),
+      mobile: new FormControl('', [
+        Validators.required,
+        Validators.pattern('[0-9]*')
+      ]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}')
+      ])
+    })
+  }
+
+  get firstname() {
+    return this.taskForm.get('firstname')
+  }
+  get mobile() {
+    return this.taskForm.get('mobile')
+  }
+  get email() {
+    return this.taskForm.get('email')
+  }
+
+  onSubmit() {
+    
+    this.taskList.push(this.taskForm.value)
+    // console.log(this.taskForm.value);
+    this.taskForm.reset();
+
+  }
+  onUpdate(){
+    this.taskList[this.editIndex] = this.taskForm.value
+    console.log(this.taskForm.value);  
+    this.isSave = true;
+    this.isUpdate = false;  
+    this.taskForm.reset()
+  }
+  onEdit(emp:any) {
+    // console.log(emp);
+    // console.log(this.taskList);
+    this.editIndex = emp;
+    
+    this.isSave = false;
+    this.isUpdate = true;
+    this.taskForm.patchValue(
+      {firstname:this.taskList[this.editIndex].firstname,
+      mobile:this.taskList[this.editIndex].mobile,
+      email:this.taskList[this.editIndex].email});
+      
+  }
+  
+  onDelete(emp:any) {
+this.taskList.splice(emp,1)
+  }
 }
